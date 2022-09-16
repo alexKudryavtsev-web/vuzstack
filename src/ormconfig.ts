7cde67aad1977198cb38b1fc1ccf50bd83b18d1f
alexKudryavtsev-web <qwertyshurazh@gmail.com>
2022-09-16 11:27:01 +0300

    fix ormconfig.ts, update README and .env.sample

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,14 +1,15 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: '../../.env' });
+
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 const config: DataSourceOptions = {
   type: 'mysql',
   host: 'localhost',
   port: 3306,
-  username: 'root',
-  password: 'redux123',
-  database: 'vuzstack',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/api/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   synchronize: false,
