dfb148346de7f6384fce3b1ed9b853796ff0507d
alexKudryavtsev-web <qwertyshurazh@gmail.com>
2022-09-17 12:43:41 +0300

    add formik for UserForm.jsx

--- frontend/src/components/userForm/UserForm.jsx
+++ frontend/src/components/userForm/UserForm.jsx
@@ -1,17 +1,89 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import {
+  validSize,
+  validText,
+  validPassportID,
+  validPassportSeries,
+  validPhone,
+  validSnils,
+  validURL,
+} from '../../utils/validate';
 
 function UserForm() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      link: '',
+      phone: '',
+      snils: '',
+      passportID: '',
+      passportSeries: '',
+    },
+    async onSubmit(data) {
+      console.log(data);
+    },
+    validate(data) {
+      const errors = {};
+
+      if (validText(data.firstName)) {
+        errors.firstName = 'Имя может содержать только кириллицу';
+      }
+      if (validSize(data.firstName)) {
+        errors.firstName = 'Имя не указано';
+      }
+
+      if (validText(data.lastName)) {
+        errors.lastName = 'Имя может содержать только кириллицу';
+      }
+      if (validSize(data.lastName)) {
+        errors.lastName = 'Фамилия не указана';
+      }
+
+      if (validURL(data.link)) {
+        errors.link = 'Невозможная ссылка';
+      }
+      if (validSize(data.link)) {
+        errors.link = 'Ссылка не указана';
+      }
+
+      if (validPhone(data.phone)) {
+        errors.phone = 'Неверный формат';
+      }
+      if (validSize(data.phone)) {
+        errors.phone = 'Не указан телефон';
+      }
+
+      if (validSnils(data.snils)) {
+        errors.snils = 'Пример: 402-653-194-78';
+      }
+
+      if (validPassportSeries(data.passportSeries)) {
+        errors.passportSeries = 'Первые четыре цифры в паспорте';
+      }
+
+      if (validPassportID(data.passportID)) {
+        errors.passportID = 'Последующие 6 цифр';
+      }
+
+      return errors;
+    },
+  });
+
   return (
     <div className="grid place-items-center">
       <div className="w-full md:w-11/12 lg:w-8/12 sm:mx-auto">
         <div className="bg-white shadow-sm">
           <div className="leading-loose">
-            <form className="">
+            <form onSubmit={formik.handleSubmit}>
               <div className="relative h-10 input-component mb-5 empty sm:inline-block mt-2 sm:w-1/2 sm:pr-1">
                 <input
-                  id="first"
-                  htmlType="text"
-                  name="first"
+                  id="firstName"
+                  htmltype="text"
+                  name="firstName"
+                  onChange={formik.handleChange}
+                  value={formik.values.firstName}
                   className="h-full w-full border-gray-300 px-2 transition-all hover:border-gray-500 focus:border-black-500 
               rounded-md focus:ring-0 group focus:outline-0 border text-sm"
                 />
@@ -19,44 +91,50 @@ function UserForm() {
                   htmlFor="first"
                   className="absolute left-2 transition-all bg-white px-1 text-black-600 text-xs top-0"
                 >
-                  Имя
+                  {formik.errors.firstName || 'Имя'}
                 </label>
               </div>
               <div className="relative h-10 input-component mb-5 empty sm:inline-block mt-2 sm:-mx-1 sm:pl-1 sm:w-1/2">
                 <input
-                  id="last"
-                  htmlType="text"
-                  name="last"
+                  id="link"
+                  htmltype="url"
+                  name="link"
+                  value={formik.link}
+                  onChange={formik.handleChange}
                   className="h-full w-full border-gray-300 px-2 transition-all hover:border-gray-500 focus:border-black-500 
             rounded-md focus:ring-0 group focus:outline-0 border text-sm"
-                />
-                <label
-                  htmlFor="last"
-                  className="absolute left-2 transition-all bg-white px-1 text-black-600 text-xs top-0"
-                >
-                  Ссылка на соц.сеть
-                </label>
-              </div>
-              <div className="relative h-10 input-component mb-5 empty sm:inline-block mt-2 sm:w-1/2 sm:pr-1">
-                <input
-                  id="link"
-                  htmlType="text"
-                  name="link"
-                  className="h-full w-full border-gray-300 px-2 transition-all hover:border-gray-500 focus:border-black-500 
-              rounded-md focus:ring-0 group focus:outline-0 border text-sm"
                 />
                 <label
                   htmlFor="link"
                   className="absolute left-2 transition-all bg-white px-1 text-black-600 text-xs top-0"
                 >
-                  Фамилия
+                  {formik.errors.link || 'Ссылка на соц.сеть'}
+                </label>
+              </div>
+              <div className="relative h-10 input-component mb-5 empty sm:inline-block mt-2 sm:w-1/2 sm:pr-1">
+                <input
+                  id="lastName"
+                  htmltype="text"
+                  name="lastName"
+                  value={formik.lastName}
+                  onChange={formik.handleChange}
+                  className="h-full w-full border-gray-300 px-2 transition-all hover:border-gray-500 focus:border-black-500 
+              rounded-md focus:ring-0 group focus:outline-0 border text-sm"
+                />
+                <label
+                  htmlFor="lastName"
+                  className="absolute left-2 transition-all bg-white px-1 text-black-600 text-xs top-0"
+                >
+                  {formik.errors.lastName || 'Фамилия'}
                 </label>
               </div>
               <div className="relative h-10 input-component mb-5 empty sm:inline-block mt-2 sm:-mx-1 sm:pl-1 sm:w-1/2">
                 <input
                   id="phone"
-                  htmlType="text"
+                  htmltype="text"
                   name="phone"
+                  value={formik.phone}
+                  onChange={formik.handleChange}
                   className="h-full w-full border-gray-300 px-2 transition-all hover:border-gray-500 focus:border-black-500 
             rounded-md focus:ring-0 group focus:outline-0 border text-sm"
                 />
@@ -64,55 +142,71 @@ function UserForm() {
                   htmlFor="phone"
                   className="absolute left-2 transition-all bg-white px-1 text-black-600 text-xs top-0"
                 >
-                  Телефон
+                  {formik.errors.phone || 'Телефон'}
                 </label>
               </div>
               <p className="text-gray-800 font-medium">СНИЛС</p>
               <div className="relative h-10 input-component mb-5 empty mt-2">
                 <input
-                  id="address"
-                  htmlType="text"
-                  name="address"
+                  id="snils"
+                  htmltype="text"
+                  name="snils"
+                  value={formik.snils}
+                  onChange={formik.handleChange}
                   className="h-full w-full border-gray-300 px-2 transition-all hover:border-gray-500 focus:border-black-500 
             rounded-md focus:ring-0 group focus:outline-0 border text-sm"
                 />
                 <label
-                  htmlFor="address"
+                  htmlFor="snils"
                   className="absolute left-2 transition-all bg-white px-1 text-black-600 text-xs top-0"
                 >
-                  Номер
+                  {formik.errors.snils || 'Номер'}
                 </label>
               </div>
               <p className="text-gray-800 font-medium">Паспорт</p>
               <div className="relative h-10 input-component mb-5 empty sm:inline-block mt-2 sm:w-1/2 sm:pr-1">
                 <input
-                  id="identitas"
-                  htmlType="text"
-                  name="identitas"
+                  id="passportSeries"
+                  htmltype="text"
+                  name="passportSeries"
+                  value={formik.passportSeries}
+                  onChange={formik.handleChange}
                   className="h-full w-full border-gray-300 px-2 transition-all hover:border-gray-500 focus:border-black-500 
               rounded-md focus:ring-0 group focus:outline-0 border text-sm"
                 />
                 <label
-                  htmlFor="idNumber"
+                  htmlFor="passportSeries"
                   className="absolute left-2 transition-all bg-white px-1 text-black-600 text-xs top-0"
                 >
-                  Серия
+                  {formik.errors.passportSeries || 'Серия'}
                 </label>
               </div>
               <div className="relative h-10 input-component mb-5 empty sm:inline-block mt-2 sm:-mx-1 sm:pl-1 sm:w-1/2">
                 <input
-                  id="idState"
-                  htmlType="text"
-                  name="identitas"
+                  id="passportID"
+                  htmltype="text"
+                  name="passportID"
+                  value={formik.passportID}
+                  onChange={formik.handleChange}
                   className="h-full w-full border-gray-300 px-2 transition-all hover:border-gray-500 focus:border-black-500 
             rounded-md focus:ring-0 group focus:outline-0 border text-sm"
                 />
                 <label
-                  htmlFor="idState"
+                  htmlFor="passportID"
                   className="absolute left-2 transition-all bg-white px-1 text-black-600 text-xs top-0"
                 >
-                  Номер
+                  {formik.errors.passportID || 'Номер'}
                 </label>
+              </div>
+              <div className="flex justify-end mt-2">
+                <button
+                  htmltype="submit"
+                  className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  type="button"
+                  onClick={formik.handleSubmit}
+                >
+                  Готово
+                </button>
               </div>
             </form>
           </div>
---
+++ frontend/src/utils/validate.js
@@ -0,0 +1,37 @@
+export function validURL(string) {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+    '((\\d{1,3}\\.){3}\\d{1,3}))' +
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+    '(\\?[;&a-z\\d%_.~+=-]*)?' +
+    '(\\#[-a-z\\d_]*)?$',
+    'i',
+  );
+
+  return !Boolean(pattern.test(string));
+}
+
+export function validPhone(string) {
+  return !/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(string);
+}
+
+export function validText(string) {
+  return !/^[а-яА-Я]+$/.test(string);
+}
+
+export function validSize(string) {
+  return !Boolean(string.trim());
+}
+
+export function validSnils(string) {
+  return !/^\d{3}-\d{3}-\d{3}-\d{2}$/.test(string);
+}
+
+export function validPassportSeries(string) {
+  return !/^\d{4}$/.test(string);
+}
+
+export function validPassportID(string) {
+  return !/^\d{6}$/.test(string);
+}
